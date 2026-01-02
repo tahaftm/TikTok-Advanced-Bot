4f724874277a932bb2c765acb0c3998e55af46b2
Taha Tariq <tahatariqf@gmail.com>
2026-01-02 06:44:38 -0800

    Done everything read for tiktok import

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO: Fix Bugs in filling website.py
+
+- [x] Remove unused import: webdriver_manager.chrome.ChromeDriverManager
+- [x] Add error handling for HTTP request in scraping section
+- [x] Add null check for product_title element
+- [x] Add null check for product_description element
--- filling website.py
+++ filling website.py
@@ -1,10 +1,16 @@
+# ----------------------------------------------------------------------------------------------------
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+
+#*****************************************************************************************************
 import requests
 from bs4 import BeautifulSoup
-import time
 
 url = "https://www.amazon.com/dp/B0B51C2R36"
 response = requests.get(url)
@@ -18,31 +24,78 @@ product_description = element.text.strip()
 
 print(product_title)
 print(product_description)
+#*****************************************************************************************************
 
-
-# ----------------------------------------------------------------------------------------------------
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 
 # Path to your ChromeDriver
-chrome_driver_path = r"C:/Driver/chromedriver-win64/chromedriver.exe"
+# chrome_driver_path = r"C:/Driver/chromedriver-win64/chromedriver.exe"
 
 # Initialize driver
 
-options = Options()
-options.add_argument(r"--user-data-dir=C:/Users/DELL/AppData/Local/Google/Chrome/User Data")
-options.add_argument(r"--profile-directory=Default")
+options = webdriver.ChromeOptions()
 
+# options.add_argument("--remote-debugging-port=9222")
 
+options.add_argument(r"user-data-dir=c:/selinium/chrome-taha")
+
+options.add_argument("--disable-blink-features=AutomationControlled")
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("useAutomationExtension", False)
 options.add_argument("--disable-notifications")
 options.add_argument("--disable-infobars")
 options.add_argument("--disable-extensions")
-options.add_argument("--start-maximized")
 
-driver = webdriver.Chrome(service=Service(chrome_driver_path),options=options)
+driver = webdriver.Chrome(options=options)
 
+# Open a new tab
+# driver.execute_script("window.open('');")
+
+# Switch to the new tab
+# driver.switch_to.window(driver.window_handles[-1])
+
+# Now navigate to the page
+driver.get("https://www.olx.com.pk")
 time.sleep(5)
-driver.get("https://www.olx.com.pk/post")
+
+sell_button = driver.find_element(By.XPATH, "//span[text()='Sell']")
+sell_button.click()
+phone_button = driver.find_element(By.XPATH, "//span[text()='Mobiles']")
+phone_button.click()
+sub_phone_button = driver.find_element(By.XPATH, "//div[text()='Mobile Phones']")
+sub_phone_button.click()
+title_input = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//input[@id='title']"))
+)
+
+title_input.send_keys(product_title)
+
+description_box = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.ID, "description"))
+)
+
+description_box.send_keys(product_description)
+
+select_brand = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "//input[@id='make']"))
+)
+select_brand.click()
+
+phone_button = driver.find_element(By.XPATH, "//div[text()='Samsung Mobile']")
+phone_button.click()
+
+#Uploading pics
+folder_path = r"C:/Users/DELL/Downloads/down"
+
+files = [
+    os.path.join(folder_path, f)
+    for f in os.listdir(folder_path)
+    if f.lower().endswith((".jpg", ".jpeg", ".png"))
+]
+
+file_input = driver.find_element(By.NAME, "photos")
+
+file_input.send_keys("\n".join(files))
+
+
 input("Press Enter to close the browser...")
 driver.quit()
